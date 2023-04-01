81915ba09c65947bae9fd4af0efbf0abec729b38
Shravan <ishravan919@gmail.com>
2023-04-02 00:15:45 +0530

    sidebar navbar added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react';
 import App from './App';
+import "./index.css";
+import { StateContextProvider } from './context';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <ThirdwebProvider desiredChainId = {ChainId.Goerli}>
         <Router>
-            <App/>
+        <StateContextProvider>
+          <App/>
+        </StateContextProvider>
+            
         </Router>
 
 
---
+++ client/src/pages/CampaignDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CampaignDetails = () => {
+  return (
+    <div>
+
+
+
+      
+    </div>
+  )
+}
+
+export default CampaignDetails
