44bd9e0a76d0a44c2e63ee6d6fe3afaa838dca1b
Aayush <aayushbisht501@gmail.com>
2023-04-01 17:43:18 +0530

    created contract and initialized client

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
+    defaultNetwork : 'goerli',
+    networks :{
+      hardhat : {},
+      goerli: {
+        url : 'https://rpc.ankr.com/eth_goerli',
+        account : [`0x${process.env.PRIVATE_KEY}`]
+      }
+
+    },
     settings: {
       optimizer: {
         enabled: true,
