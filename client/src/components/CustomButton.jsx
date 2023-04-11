8f8ba2fd4ca0bba7f19e791f6685ce5423bac0e7
Aayush <aayushbisht501@gmail.com>
2023-04-11 11:59:24 +0530

    changes

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -4,7 +4,7 @@ const CustomButton = ({ btnType, title, handleClick, styles }) => {
   return (
     <button
       type={btnType}
-      className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[42px] px-4 rounded-[28px] ${styles}`}
+      className={`font-epilogue font-semibold text-[14px] leading-[26px] text-white min-h-[40px] mx-h-[20px] px-4 rounded-[28px] ${styles}`}
       onClick={handleClick}
     >
       {title}
