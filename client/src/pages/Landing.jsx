b2722c50e0dbdfa9e2a7975e08d2d32fd4587f8f
Shravan <ishravan919@gmail.com>
2023-05-10 15:30:07 +0530

    linking in landing page sort

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,45 +1,76 @@
-import React from 'react'
-import {useNavigate} from 'react-router-dom'
-import Land from './Land.png'
+import React from "react";
+import Land4 from "./land4.png";
+import Logo from "./krypti.png";
+import "animate.css/animate.min.css";
+import Typist from "react-typist";
 
 export default function Landing({onHide}) {
-  
+  const backgroundImageStyle = {
+    backgroundImage: `linear-gradient(rgba(0,0,0,0.8) 0%, rgba(0,0,0,0.6) 50%, rgba(0,0,0,0.8) 100%), url(${Land4})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    minHeight: "100vh",
+  };
+  const logoStyle = {
+    height: "90px",
+    width: "auto",
+    background: `url(${Logo}) no-repeat`,
+    backgroundSize: "contain",
+    top: "0%",
+    left: "50%",
+    zIndex: "1",
+    marginLeft: "25px",
+  };
+
   return (
-    <div
-    style={{
-        backgroundImage: `url(${Land})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor: 'rgba(255, 255, 255, 0.8)',
-        minHeight: '100vh',
-      }}
-      >
-     
-        
-<nav class="bg-transparent border-gray-200 dark:bg-gray-900">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="https://flowbite.com/" class="flex items-center">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">KryptoRaise</span>
-    </a>
-   
-  </div>
-</nav>
+    <div style={backgroundImageStyle}>
+      <div style={logoStyle}></div>
+      <section className="text-gray-600 body-font relative">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col ml-20 w-full mb-12 animate_animated animate_fadeIn">
+            <div className="flex flex-col w-full mb-12">
+              <h3 className="sm:text-5xl text-9xl font-bold title-font mb-4 text-white " style={{ fontWeight:900 }}>
+                Revolutionizing <span style={{ color: "#60F", fontWeight:900 }}>Crowdfunding</span> with <span style={{ color: "#60F", fontWeight:900 }}>Blockchain</span> !
+              </h3>
+              <h1
+                className="lg:w-2/3 text-3xl  font-bold leading-relaxed text-white"
+                style={{
+                  backgroundClip: "text",
+                  textFillColor: "transparent",
+                  textShadow: "2px 2px #000",
+                }}
+              >
+                Say Goodbye to Intermediaries
+              </h1>
+              <h1
+                className="lg:w-2/3 text-3xl  font-bold leading-relaxed text-white"
+                style={{
+                  backgroundClip: "text",
+                  textFillColor: "transparent",
+                  textShadow: "2px 2px #000",
+                }}
+              >              <Typist avgTypingDelay={80} cursor={{ hideWhenDone: true }}>
 
-<section class="text-gray-600 body-font relative">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-col text-center w-full mb-12">
-      <h1 class="sm:text-5xl text-9xl font-medium title-font mb-4 text-white">KryptoRaise</h1>
-      <p class="lg:w-2/3 text-2xl mx-auto leading-relaxed  text-white">KryptoRaise is a blockchain-based crowdfunding application which enables secure,transparent and decentralized funding of projects eliminating intermediaries and offering trustworthy transactions through blockchain technology.</p>
-    </div>
-    <div className="flex justify-center">
-  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={onHide}>
-    Get Started!
-  </button>
-</div>
+                And Hello to {" "}
+                  <span style={{ color: "#60F", fontWeight:900,fontSize:"35px" }}>KryptoRaise!</span>
+                  <Typist.Backspace count={12} delay={1000} />
+                  <span style={{ color: "#60F", fontWeight: 900,fontSize:"35px" }}>KryptoRaise!</span>
 
+                  </Typist>
+
+                </h1>
+              <div className="flex mt-8">
+              <button className=" bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full" onClick={onHide}>
+  Get Started!
+</button>
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-    </section>
-    </div>
-  )
+  );
 }
